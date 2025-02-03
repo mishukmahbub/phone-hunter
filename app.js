fa79572f379f7c3789b4de11564d346327517230
mishukmahbub <mishukmahbub@gmail.com>
2025-02-02 20:30:09 -0800

    all phones data search result done

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 // load data from api
-const loadPhoneData = async () => {
+const loadPhoneData = async (searchText, isShowAll) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phonesData = data.data;
-  displayPhoneData(phonesData);
+  displayPhoneData(phonesData, isShowAll);
 };
 // loadPhoneData();
 
 // display phone data
-const cardContainer = document.getElementById("card-container");
-const displayPhoneData = (phones) => {
+const displayPhoneData = (phones, isShowAll) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerText = "";
+  const btnShowAll = document.getElementById("btn-showAll");
+  if (phones.length > 12 && !isShowAll) {
+    btnShowAll.classList.remove("hidden");
+    phones = phones.slice(0, 12);
+  } else {
+    btnShowAll.classList.add("hidden");
+  }
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
 
     const cardDiv = document.createElement("div");
     cardDiv.classList.add("card", "w-96", "bg-base-100", "shadow-xl", "mb-6");
@@ -26,12 +34,33 @@ const displayPhoneData = (phones) => {
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>${phone.brand}</p>
             <div class="card-actions justify-end">
-              <button class="btn btn-primary">Buy Now</button>
+              <button class="btn btn-primary">Show Details</button>
             </div>
           </div>
     `;
     cardContainer.appendChild(cardDiv);
   });
+  handleSpinner(false);
 };
 
-loadPhoneData();
+const handleSearch = (isShowAll) => {
+  handleSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadPhoneData(searchText, isShowAll);
+};
+
+const handleSpinner = (isLoading) => {
+  const spinner = document.getElementById("spinner");
+  if (isLoading) {
+    spinner.classList.remove("hidden");
+  } else {
+    spinner.classList.add("hidden");
+  }
+};
+
+const handleShowAll = () => {
+  handleSearch(true);
+};
+
+// loadPhoneData("a");
